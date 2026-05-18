555b27cd3888f03d0972f4bfaf73018996ae1156
Jakub Kaczor <j.kaczor06@gmail.com>
2026-05-18 17:22:28 +0200

    added player controller with input

---
+++ Source/GASCrash/Private/Player/GC_PlayerController.cpp
@@ -0,0 +1,68 @@
+﻿// Copyright Jakub Kaczor
+
+
+#include "GASCrash/Public/Player/GC_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "GameFramework/Character.h"
+
+void AGC_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputLocalPlayerSubsystem* InputSubsystem{ ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()) };
+	if (!IsValid(InputSubsystem)) return;
+	
+	for (UInputMappingContext* Context : InputMappingContexts)
+	{
+		InputSubsystem->AddMappingContext(Context, 0);
+	}
+	
+	UEnhancedInputComponent* EIC{ Cast<UEnhancedInputComponent>(InputComponent) };
+	if (!IsValid(EIC)) return;
+	
+	EIC->BindAction(JumpAction, ETriggerEvent::Started, this, &AGC_PlayerController::Jump);
+	EIC->BindAction(JumpAction, ETriggerEvent::Completed, this, &AGC_PlayerController::StopJumping);
+	EIC->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AGC_PlayerController::Move);
+	EIC->BindAction(LookAction, ETriggerEvent::Triggered, this, &AGC_PlayerController::Look);
+	
+}
+
+void AGC_PlayerController::Jump()
+{
+	if (!IsValid(GetCharacter())) return;
+	
+	GetCharacter()->Jump();
+}
+
+void AGC_PlayerController::StopJumping()
+{
+	if (!IsValid(GetCharacter())) return;
+	
+	GetCharacter()->StopJumping();
+}
+
+void AGC_PlayerController::Move(const FInputActionValue& Value)
+{
+	if (!IsValid(GetPawn())) return;
+	
+	const FVector2D MovementVector{ Value.Get<FVector2D>() };
+	
+	// Find which way is forward
+	const FRotator YawRotation(0.f, GetControlRotation().Yaw, 0.f);
+	const FVector ForwardDirection{ FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X) };
+	const FVector RightDirection{ FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y) };
+	
+	GetPawn()->AddMovementInput(ForwardDirection, MovementVector.Y);
+	GetPawn()->AddMovementInput(RightDirection, MovementVector.X);
+}
+
+void AGC_PlayerController::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisVector{ Value.Get<FVector2D>() };
+	
+	AddYawInput(LookAxisVector.X);
+	AddPitchInput(LookAxisVector.Y);
+}
---
+++ Source/GASCrash/Public/Player/GC_PlayerController.h
@@ -0,0 +1,38 @@
+﻿// Copyright Jakub Kaczor
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GC_PlayerController.generated.h"
+
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class GASCRASH_API AGC_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void SetupInputComponent() override;	
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Input")
+	TArray<TObjectPtr<UInputMappingContext>> InputMappingContexts;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Input")
+	TObjectPtr<UInputAction> JumpAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Input")
+	TObjectPtr<UInputAction> LookAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "GAS|Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	void Jump();
+	void StopJumping();
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+};
