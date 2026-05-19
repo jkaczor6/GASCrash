ec68c3102e0da298c1617288fd521ccb4608f554
Jakub Kaczor <j.kaczor06@gmail.com>
2026-05-19 18:47:30 +0200

    added gameplay tags

---
+++ Source/GASCrash/Private/GameplayTags/GC_Tags.cpp
@@ -0,0 +1,9 @@
+﻿#include "GameplayTags/GCTags.h"
+
+namespace GCTags
+{
+	namespace GCAbilities
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Primary, "GCTags.GCAbilities.Primary", "Tag for the Primary Ability")
+	}
+}
