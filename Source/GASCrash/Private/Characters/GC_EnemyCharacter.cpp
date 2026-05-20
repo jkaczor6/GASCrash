4d90af3d6e3bff9d6a728090768737af4ff1bed0
Jakub Kaczor <j.kaczor06@gmail.com>
2026-05-20 20:36:24 +0200

    added enemy class

---
+++ Source/GASCrash/Private/Characters/GC_EnemyCharacter.cpp
@@ -0,0 +1,33 @@
+﻿// Copyright Jakub Kaczor
+
+
+#include "Characters/GC_EnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+AGC_EnemyCharacter::AGC_EnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+	
+}
+
+UAbilitySystemComponent* AGC_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AGC_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (!IsValid(GetAbilitySystemComponent())) return;
+	
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this, this);
+	if (!HasAuthority()) return;
+	
+	GiveStartupAbilities();
+}
---
+++ Source/GASCrash/Public/Characters/GC_EnemyCharacter.h
@@ -0,0 +1,28 @@
+﻿// Copyright Jakub Kaczor
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GC_BaseCharacter.h"
+#include "GC_EnemyCharacter.generated.h"
+
+class UAbilitySystemComponent;
+
+UCLASS()
+class GASCRASH_API AGC_EnemyCharacter : public AGC_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AGC_EnemyCharacter();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+};
