393bd7535666b1c4875aec228a8561cf56d0c417
Jakub Kaczor <j.kaczor06@gmail.com>
2026-05-20 21:43:05 +0200

    overlap test

---
+++ Source/GASCrash/Public/AbilitySystem/Abilities/Player/GC_Primary.h
@@ -0,0 +1,28 @@
+﻿// Copyright Jakub Kaczor
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/GC_GameplayAbility.h"
+#include "GC_Primary.generated.h"
+
+UCLASS()
+class GASCRASH_API UGC_Primary : public UGC_GameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category="GAS|Abilities")
+	void HitBoxOverlapTest();
+	
+private:
+	
+	UPROPERTY(EditDefaultsOnly, Category="GAS|Abilities")
+	float HitBoxRadius{ 100.f };
+	
+	UPROPERTY(EditDefaultsOnly, Category="GAS|Abilities")
+	float HitBoxForwardOffset{ 200.f };
+	
+	UPROPERTY(EditDefaultsOnly, Category="GAS|Abilities")
+	float HitBoxElevationOffset{ 20.f };
+};
