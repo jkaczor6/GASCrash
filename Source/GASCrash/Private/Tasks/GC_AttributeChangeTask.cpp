0d4a54ae412186d3f6b4a2fe65e4d093fb31997c
Jakub Kaczor <j.kaczor06@gmail.com>
2026-05-22 14:50:15 +0200

    created attribute change async task

---
+++ Source/GASCrash/Private/Tasks/GC_AttributeChangeTask.cpp
@@ -0,0 +1,39 @@
+﻿// Copyright Jakub Kaczor
+
+
+#include "Tasks/GC_AttributeChangeTask.h"
+#include "AbilitySystemComponent.h"
+
+UGC_AttributeChangeTask* UGC_AttributeChangeTask::ListenForAttributeChange(
+	UAbilitySystemComponent* AbilitySystemComponent, FGameplayAttribute Attribute)
+{
+	UGC_AttributeChangeTask* WaitForAttributeChangeTask{ NewObject<UGC_AttributeChangeTask>() };
+	WaitForAttributeChangeTask->ASC = AbilitySystemComponent;
+	WaitForAttributeChangeTask->AttributeToListenFor = Attribute;
+	
+	if (!IsValid(AbilitySystemComponent))
+	{
+		WaitForAttributeChangeTask->RemoveFromRoot();
+		return nullptr;
+	}
+	
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Attribute).AddUObject(WaitForAttributeChangeTask, &ThisClass::AttributeChanged);
+	
+	return WaitForAttributeChangeTask;
+}
+
+void UGC_AttributeChangeTask::EndTask()
+{
+	if (ASC.IsValid())
+	{
+		ASC->GetGameplayAttributeValueChangeDelegate(AttributeToListenFor).RemoveAll(this);
+	}
+	
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UGC_AttributeChangeTask::AttributeChanged(const FOnAttributeChangeData& Data)
+{
+	OnAttributeChanged.Broadcast(Data.Attribute, Data.NewValue, Data.OldValue);
+}
