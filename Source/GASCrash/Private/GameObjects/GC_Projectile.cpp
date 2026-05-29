f776f7cc4ed7c28e451be82464a984ca9975afe7
Jakub Kaczor <j.kaczor06@gmail.com>
2026-05-29 18:11:43 +0200

    Update .gitignore

---
+++ Source/GASCrash/Private/GameObjects/GC_Projectile.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright Jakub Kaczor
+
+
+#include "GameObjects/GC_Projectile.h"
+
+
+// Sets default values
+AGC_Projectile::AGC_Projectile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AGC_Projectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGC_Projectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/GASCrash/Public/GameObjects/GC_Projectile.h
@@ -0,0 +1,25 @@
+﻿// Copyright Jakub Kaczor
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GC_Projectile.generated.h"
+
+UCLASS()
+class GASCRASH_API AGC_Projectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AGC_Projectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
